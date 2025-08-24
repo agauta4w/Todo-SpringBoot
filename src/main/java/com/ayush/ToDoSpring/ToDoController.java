565bdabfbcdd09bc2182d97d4d110a9ae3718267
ayush <ayushgautam2150@gmail.com>
2025-08-25 00:00:26 +0530

    Added todo creatios

--- src/main/java/com/ayush/ToDoSpring/ToDoController.java
+++ src/main/java/com/ayush/ToDoSpring/ToDoController.java
@@ -1,6 +1,6 @@
 package com.ayush.ToDoSpring;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +8,27 @@ import java.util.List;
 @RestController
 public class ToDoController {
 
-    private static List<ToDo> todos;
+    private static List<ToDo> todoList;
 
     public ToDoController(){
-        todos = new ArrayList<>();
-        todos.add(new ToDo(1,false,"Todo 1",1));
-        todos.add(new ToDo(2,false,"Todo 2",2));
-        todos.add(new ToDo(3,false,"Todo 3",3));
+        todoList = new ArrayList<>();
+        todoList.add(new ToDo(1,false,"Todo 1",1));
+        todoList.add(new ToDo(2,false,"Todo 2",2));
+        todoList.add(new ToDo(3,false,"Todo 3",3));
 
 
     }
+
+    @GetMapping("/todos")
+    public List<ToDo> getTodos(){
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public ToDo createTodo(@RequestBody ToDo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
+    }
+
 
 }
